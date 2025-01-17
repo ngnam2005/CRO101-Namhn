bdb2b39e0c5f85aa72150c1a4ea4599323c11392
ngnam2005 <namhnpd09895@gmail.com>
2025-01-17 12:58:52 +0700

    Lab3

--- Lab345678/lab3/index.js
+++ Lab345678/lab3/index.js
@@ -1,20 +1,153 @@
-import { StyleSheet, Text, View, TextInput } from "react-native";
+import { Pressable, ScrollView, SafeAreaView, SafeAreaProvider,Image, Modal, StyleSheet, Text, TextInput, View, Alert } from "react-native";
 import { styles } from "./styles";
-import { useState } from "react";
+import React, { useState } from "react";
+const colorText = (color) => {
+    return { color: color };
+}
+
+const sizeText = (size) => {
+    return { fontSize: size };
+}
+
+const styleText = (additionalStyles) => {
+    return { ...additionalStyles };
+}
+
+
 const Lab3 = () => {
-  const [name, setName] = useState("");
-  return (
-    <View style={styles.container}>
-      <TextInput
-        value={name}
-        onChangeText={setName}
-        placeholder="Nhap ho ten"
-        style={styles.tipStyle}
-      />
-      <Text style={styles.baseText}>
-        Em vao doi bang <Text style={styles.boldText}>vang do</Text>
-      </Text>
-    </View>
-  );
+    const [name, setName] = useState('');
+    const [phone, setPhone] = useState('');
+    const [pass, setPass] = useState('');
+
+    const [modalVisible, setModalVisible] = useState(false);
+
+
+    return (
+        <ScrollView style={styleText({
+            backgroundColor: 'white'
+        })}>
+            <TextInput
+                value={name}
+                onChange={setName}
+                placeholder="Nhập họ tên"
+                style={styles.tipStyle}
+            >
+            </TextInput>
+            <TextInput
+                value={phone}
+                onChange={setPhone}
+                placeholder="Nhập số điện thoại"
+                keyboardType="phone-pad"
+                style={styles.tipStyle}
+            >
+            </TextInput>
+            <TextInput
+                value={pass}
+                onChange={setPass}
+                placeholder="Nhập mật khẩu"
+                secureTextEntry={true}
+                style={styles.tipStyle}
+            >
+            </TextInput>
+            <View style={styles.container}>
+                <Text style={styles.baseText}>
+                    Em vào đời bằng <Text style={[styles.boldText, colorText('red')]}>vang do</Text> anh vào đời bằng {' '}
+                    <Text style={[styles.boldText, colorText('yellow')]}>nước trà</Text>
+                </Text>
+                {/* line 2 */}
+                <Text style={styles.baseText}>
+                    Bằng cơn mưa thơm <Text style={[styles.boldText, sizeText(20), styles.italicText]}>mùi đất</Text>{' '}
+                    và <Text style={sizeText(10)}>hoa dại mọc trước nhà</Text>
+                </Text>
+                {/* line 3 */}
+                <Text style={[styles.baseText, styles.boldText, styles.italicText, colorText('gray')]}>
+                    Em vào đời bằng kế hoạch, anh vào đời bằng mộng mơ
+                </Text>
+                {/* line 4 */}
+                <Text style={styles.baseText}>
+                    Lý trí em là {' '}
+                    <Text style={styleText({
+                        textDecorationLine: 'underline',
+                        letterSpacing: 20,
+                        fontWeith: 'bold',
+
+                    })}>
+                        công cụ
+                    </Text>
+                    còn trái tim anh {' '}
+                    <Text style={styleText({
+                        textDecorationLine: 'underline',
+                        letterSpacing: 20,
+                        fontWeith: 'bold',
+                    })}>
+                        động cơ
+                    </Text>
+                </Text>
+                {/*Line 5*/}
+                <Text style={[styles.baseText, styleText({ textAlign: 'right' })]}>
+                    Em vào đời nhiều đồng nghiệp anh vào đời nhiều thân tình
+                </Text>
+                {/*Line 6*/}
+                <Text style={[styles.baseText, styleText({
+                    textAlign: 'center',
+                    fontWeith: 'bold',
+                    color: 'orange'
+                })]}>
+                    Em vào đời nhiều đồng nghiệp anh vào đời nhiều thân tình
+                </Text>
+                {/*Line 7*/}
+                <Text style={[styles.baseText, styleText({
+                    fontWeith: 'bold',
+                    color: 'black',
+                })]}>
+                    Em vào đời bằng <Text style={colorText('white')}> mấy trắng </Text> em
+                    vào đời bằng <Text style={colorText('yellow')}>nắng xanh</Text>
+                </Text>
+                {/*Line 7*/}
+                <Text style={[styles.baseText, styleText({
+                    fontWeith: 'bold',
+                    color: 'black',
+                })]}>
+                    Em vào đời bằng <Text style={colorText('yellow')}> đại lộ </Text> và
+                    con đường đó giờ <Text style={colorText('white')}>vắng anh</Text>
+                </Text>
+            </View>
+            <SafeAreaView style= {styles.containerModal}>
+                    
+                <Modal animationType="slide"
+                    transparent={true}
+                    visible={modalVisible}
+                    onRequestClose={() => {
+                    Alert.alert('Model has been closed ');
+                    setModalVisible(!modalVisible);
+                    }}>
+                <View style={styles.modalView}>
+                        <Text style={[styleText({marginBottom: 20}), styles.boldText]}>
+                            Hello Wolrd!
+                        </Text>
+                        <Image source={require('./image/img.png')} style={styleText({height: 200, width:200})} />
+                        <Pressable style={[styles.btn, styles.btnClose,styleText({ marginTop: 20, width: 150, alignSelf: 'center' })]}
+                        onPress={()=>{
+                            setModalVisible(!modalVisible)
+                        }}
+                        >
+                            <Text  style={styleText({ textAlign: 'center' })}>Close Modal</Text>
+                        </Pressable>
+                        </View>
+                    </Modal>
+                    
+                </SafeAreaView>
+            
+            <Pressable style={[styles.btn, styles.btnOpen, styleText({ marginTop: 20, width: 150, alignSelf: 'center' })]}
+                onPress={() => {
+                    setModalVisible(true)
+                }}
+            >
+                <Text style={styleText({ textAlign: 'center' })}>Show Modal</Text>
+            </Pressable>
+           
+        </ScrollView>
+    );
 };
+
 export default Lab3;
--- Lab345678/lab3/styles.js
+++ Lab345678/lab3/styles.js
@@ -1,25 +1,66 @@
-import {StyleSheet} from 'react-native';
+import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
-  container: {
-    backgroundColor: 'blue',
-    width: '90%',
-    alignSelf: 'center',
-    marginTop: 20,
-    padding: 15,
-  },
-  baseText: {
-    fontFamily: 'Cochin',
-    color: 'whilte',
-  },
-  boldText: {
-    fontWeight: 'bold',
-  },
-  tipStyle: {
-    borderWidth: 1,
-    borderColor: 'gray',
-    borderRadius: 10,
-    marginHorizontal: 15,
-    marginTop: 10,
-  },
+    container: {
+        backgroundColor: 'blue',
+        width: '90%',
+        alignSelf: 'center',
+        marginTop: 20,
+        padding: 15,
+        fontFamily: 'Cochin',
+    },
+    baseText: {
+        color: 'white',
+        fontSize: 16,
+        marginTop: 10
+    },
+    boldText: {
+        fontWeight: 'bold',
+    },
+    tipStyle: {
+        borderWidth: 1,
+        borderColor: 'gray',
+        borderRadius: 10,
+        marginHorizontal: 15,
+        marginTop: 30,
+        padding: 20
+    },
+    italicText: {
+        fontStyle: 'italic'
+    },
+    btnOpen: {
+        backgroundColor: '#F194FF'
+    },
+    btnClose: {
+        backgroundColor: '#F194FF'
+    },
+    btn: {
+        borderRadius: 20,
+        padding: 10,
+        elevation: 2,
+    },
+    containerModal: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#f5f5f5',
+      },
+      modalView: {
+        margin: 20,
+        marginTop: 100,
+        backgroundColor: 'white',
+        borderRadius: 10,
+        padding: 20,
+        alignItems: 'center',
+        shadowColor: '#000',
+        shadowOffset: {
+          width: 0,
+          height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5,
+        borderWidth: 1,
+        borderColor: '#ccc',
+      },
 });
